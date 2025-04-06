d77d8d482692aa028b42bdcc9a92ebf87e201563
pumpumx <trishuchibhot14@gmail.com>
2025-04-06 20:02:41 +0530

    Production Done

--- src/Components/Home/Attendance/Profile.jsx
+++ src/Components/Home/Attendance/Profile.jsx
@@ -4,7 +4,7 @@
     return (
       <div className='w-full h-full flex  object-fill relative justify-around items-center'>
           <div className='object-cover w-[8%] rounded-[100%] h-[80%]  flex items-center justify-center overflow-hidden'>
-              <img className='object-cover rounded-[100%]' src="src/assets/download.jpeg" alt="Pfp"  />
+              <img className='object-cover rounded-[100%]' src="images/download.jpeg" alt="Pfp"  />
           </div>
           <div className=' gap-2 text-lg w-[10%] h-[50%] flex flex-col justify-center items-center'>
             <div className='flex items-center justify-center gap-2'><p className='text-xl'>Welcome,</p>
